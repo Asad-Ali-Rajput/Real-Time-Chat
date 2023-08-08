74640b68528edeff8a8d40e4b3000486d81ca642
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-08-09 01:24:55 +0500

    Real Time Chat App

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
+import LoginComponent from "./loginComponent";
+import SignupComponent from "./SignupComponent";
+
 
 function HomeComponent() {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
+
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+    setIsSignupModalOpen(false);
+  };
+
+
+  const openSignupModal = () => {
+    setIsSignupModalOpen(true);
+    setIsLoginModalOpen(false);
+  };
+
   return (
-    <div className="w-screen h-screen bg-slate-800">
-        <Navbar />
-      <div className="rounded-lg h-full text-center bg-slate-300 m-4">
-        <h1 className="text-[32px] text-slate-900 my-5">
-            Real Time Chat
-        </h1>
-      </div>
+    <div className="w-full h-full">
+        <Navbar login={openLoginModal} signup={openSignupModal}/>
+        
+        {isLoginModalOpen ? (
+        <LoginComponent />
+      ): isSignupModalOpen ? (
+        <SignupComponent />
+      ) : (<div className="rounded-lg pt-10 text-center ">
+      <h1 className="text-[32px] text-slate-200 my-4">
+          Real Time Chat
+      </h1>
+    </div>)}
     </div>
   );
 }
